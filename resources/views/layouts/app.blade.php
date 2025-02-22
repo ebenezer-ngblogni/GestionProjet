890221ac26f79b8ee33560dbf6c4800f2aa4c0ba
ebenezer-ngblogni <ebengamer6@gmail.com>
2025-02-22 17:10:43 +0100

    [ADD] Création du projet mise en place du layout, de Tailwind et des migrations de projects et tasks

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title', 'Gestion de Projets')</title>
+    {{-- <script src="https://cdn.tailwindcss.com"></script> --}}
+    <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
+</head>
+
+<body class="bg-gray-100">
+    @yield('content')
+</body>
+
+</html>
